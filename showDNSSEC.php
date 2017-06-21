b88d49653984aeb5799051c042bf5341a671d48b
Lee Keitel <lfkeitel@users.noreply.github.com>
2017-06-21 15:03:55 -0500

    Use alternative syntax for PHP foreach
    Also cleanup HTML a bit.

--- showDNSSEC.php
+++ showDNSSEC.php
@@ -1,37 +1,40 @@
 <?php
   include('config.php');
   // URL for API request
-  $sld = $_GET['sld'];
-  $tld = $_GET['tld'];
+  $sld = urlencode($_GET['sld']);
+  $tld = urlencode($_GET['tld']);
   $url = "https://$server/interface.asp?command=GetDnsSec&uid=$username&pw=$password&responsetype=xml&sld=$sld&tld=$tld";
   // Load the API results into a SimpleXML object
   $xml = simplexml_load_file($url);
+  $keylist = $xml->DnsSecData->KeyData;
 ?>
 <html>
- <head>
-  <title>Domain Name Manager</title>
- </head>
- <body>
-  <h1>Domain Name Manager</h1>
-  <h3>DNSSEC records for <?php echo "$sld.$tld"; ?></h3>
-  <table>
-   <tr>
-    <td>Key Tag</td>
-    <td>Algorithm</td>
-    <td>Digest Type</td>
-    <td>Digest</td>
-    <td>Show DNSSEC Records</td>
-   </tr>
-<?php
-$keylist = $xml->DnsSecData->KeyData;
+  <head>
+    <title>Domain Name Manager</title>
+  </head>
+  <body>
+    <h1>Domain Name Manager</h1>
+    <h3>DNSSEC records for <?= "$sld.$tld" ?></h3>
+    <table>
+      <tr>
+        <th>Key Tag</th>
+        <th>Algorithm</th>
+        <th>Digest Type</th>
+        <th>Digest</th>
+        <th>Show DNSSEC Records</th>
+      </tr>
 
-foreach ($keylist as $key) {
-  echo "<tr><td>$key->KeyTag</td><td>$key->Algorithm</td><td>$key->DigestType</td><td>$key->Digest</td><td><a href=\"deleteDNSSEC.php?sld=$sld&tld=$tld\">Show DNSSEC</a></td></tr>";
-}
-?>
-  </table>
-  <br />
-  <a href="listDomains.php">Return to Domain List</a>
- </body>
+      <?php foreach ($keylist as $key): ?>
+      <tr>
+        <td><?= $key->KeyTag ?></td>
+        <td><?= $key->Algorithm ?></td>
+        <td><?= $key->DigestType ?></td>
+        <td><?= $key->Digest ?></td>
+        <td><a href="deleteDNSSEC.php?sld=<?= $sld ?>&tld=<?= $tld ?>">Show DNSSEC</a></td>
+      </tr>
+      <?php endforeach; ?>
+    </table>
+    <br />
+    <a href="listDomains.php">Return to Domain List</a>
+  </body>
 </html>
-
