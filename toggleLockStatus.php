686cba16cc8706c105d304fbe29e0252f59d1173
Nick Bebout <nb@nb.zone>
2017-12-10 15:39:44 -0600

    Add toggleLockStatus.php script

---
+++ toggleLockStatus.php
@@ -0,0 +1,14 @@
+<?php
+require_once('init.php');
+
+// $_REQUEST contains $_POST, $_GET, and $_COOKIE
+$domain = $_REQUEST['domain'];
+$registrar = $_REQUEST['registrar'];
+
+  if ($registrar == 'enom') {
+    $clients['enom']->ToggleLocked($domain);
+  } else if ($registrar == 'namecheap') {
+    $clients['namecheap']->ToggleLocked($domain);
+  }
+header("Location: index.php");
+?>
