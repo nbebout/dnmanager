2eae575eb9b3c426b62060625ef7b91cc2204f9e
Nick Bebout <nb@nb.zone>
2017-12-10 14:36:57 -0600

    Add NameCheap pricing

--- pricing.php
+++ pricing.php
@@ -1,7 +1,9 @@
 <?php
 require_once('init.php');
 
-$tldarray = [com, net, org, mba, biz, cloud, info, mobi, name, us, zone, email, family];
+#$tldarray = [com, net, org, mba, biz, cloud, info, mobi, us, zone, email, family];
+$tldarray = [biz, cloud, co, com, email, family, info, mba, me, mobi, name, net, online, org, site, tech, us, website, zone];
+//$tldarray = [com];
 sort($tldarray);
 ?>
 <!DOCTYPE html>
@@ -19,20 +21,26 @@ sort($tldarray);
   </head>
 
   <body>
-    <h1>Domain Pricing - eNom</h1>
+    <h1>Domain Pricing</h1>
     <table>
       <tr>
         <th>TLD</th>
-        <th>Register</th>
-        <th>Renew</th>
-        <th>Transfer</th>
+        <th>eNom Register</th>
+        <th>eNom Renew</th>
+        <th>eNom Transfer</th>
+        <th>NameCheap Register</th>
+        <th>NameCheap Renew</th>
+        <th>NameCheap Transfer</th>
       </tr>
-      <?php foreach ($tldarray as $tld): ?>
+      <?php setlocale(LC_MONETARY, 'en_US.UTF-8'); foreach ($tldarray as $tld): ?>
         <tr>
           <td><?= $tld; ?></td>
-          <td>$<?= $clients['enom']->GetResellerPrice('new', $tld); ?></td>
-          <td>$<?= $clients['enom']->GetResellerPrice('renew', $tld); ?></td>
-          <td>$<?= $clients['enom']->GetResellerPrice('transfer', $tld); ?></td>
+          <td><?= money_format('%.2n', $clients['enom']->GetResellerPrice('new', $tld)); ?></td>
+          <td><?= money_format('%.2n', $clients['enom']->GetResellerPrice('renew', $tld)); ?></td>
+          <td><?= money_format('%.2n', $clients['enom']->GetResellerPrice('transfer', $tld)); ?></td>
+          <td><?= money_format('%.2n', $clients['namecheap']->GetResellerPrice('REGISTER', $tld)); ?></td>
+          <td><?= money_format('%.2n', $clients['namecheap']->GetResellerPrice('RENEW', $tld)); ?></td>
+          <td><?= money_format('%.2n', $clients['namecheap']->GetResellerPrice('TRANSFER', $tld)); ?></td>
         </tr>
       <?php endforeach; ?>
   </body>
