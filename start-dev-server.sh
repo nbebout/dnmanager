9e16f29d0c5ce0d0651df8b596a9aeca993da9a1
Lee Keitel <lee@keitel.xyz>
2023-10-27 12:47:10 -0500

    Fixed dev server

--- start-dev-server.sh
+++ start-dev-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker network create dnmanager 2>/dev/null || true
-docker run -d -p 8080:80 --name dnmanager -v "$DIR":/var/www/html --network dnmanager php:7.0-apache
+docker build -f Dockerfile.dev -t php-with-intl .
+
+docker run -d -p 8080:80 --name dnmanager -v "$DIR":/var/www/html php-with-intl
 
 echo "Open a browser and go to: http://localhost:8080"
